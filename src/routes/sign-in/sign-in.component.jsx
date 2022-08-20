587031eb0f77f6f9eea3cbd2a05f6bc47611cd7a
shahar.shoshany <shahar9772@gmail.com>
2022-08-20 15:47:30 +0300

    Sign In with Redirect method removed

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,40 +1,18 @@
-import { useEffect } from 'react';
-import { getRedirectResult } from 'firebase/auth';
-
 import {
-  auth,
-  signInWithGoogleRedirect,
   signInWithGooglePopup,
   createUserDocumentFromAuth,
 } from '../../utils/firebase/firebase.utils';
 
 const SignIn = () => {
-  useEffect(() => {
-    (async () => {
-      const response = await getRedirectResult(auth);
-      if (response) {
-        const userDocRef = await createUserDocumentFromAuth(response.user);
-      }
-    })();
-  }, []);
-
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
     const userDocRef = await createUserDocumentFromAuth(user);
-  };
-
-  const logGoogRedirectUser = async () => {
-    const { user } = await signInWithGoogleRedirect();
-    console.log(user);
   };
 
   return (
     <div>
       <h1>Sign In Page</h1>
       <button onClick={logGoogleUser}>Sign in with Google Popup</button>
-      <button onClick={signInWithGoogleRedirect}>
-        Sign in with Google Redirect method
-      </button>
     </div>
   );
 };
