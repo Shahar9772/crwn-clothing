75e5a59847c5cbce5260ad02ece66c26010515c6
shahar.shoshany <shahar9772@gmail.com>
2022-09-17 21:26:22 +0300

    update contexts file names

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
-import { UserProvider } from './contexts/user.contexts';
-import { ProductsProvider } from './contexts/products.contexts';
+import { UserProvider } from './contexts/user.context';
+import { ProductsProvider } from './contexts/products.context';
 
 import App from './App';
 
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -5,7 +5,7 @@ import CartIcon from '../../components/cart-icon/cart-icon.component';
 import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
 
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
-import { UserContext } from '../../contexts/user.contexts';
+import { UserContext } from '../../contexts/user.context';
 import { signOutUser } from '../../utils/firebase/firebase.utils';
 
 import './navigation.styles.scss';
