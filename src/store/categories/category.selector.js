9dd34e3d9573216ffc5040d39d29cefcbc1d1a6b
shahar.shoshany <shahar9772@gmail.com>
2022-12-08 20:23:20 +0200

    adding createSelector

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -1,8 +1,26 @@
-export const selectCategoriesMap = (state) => {
-  console.log('categoriesMap selector fired');
-  return state.categories.categories.reduce((acc, category) => {
-    const { title, items } = category;
-    acc[title.toLowerCase()] = items;
-    return acc;
-  }, {});
+import { createSelector } from 'reselect';
+
+const selectCategoryReducer = (state) => {
+  console.log('selector 1 fired');
+  return state.categories;
 };
+
+const selectCategories = createSelector(
+  [selectCategoryReducer],
+  (categoriesSlicer) => {
+    console.log('selector 2 fired');
+    return categoriesSlicer.categories;
+  }
+);
+
+export const selectCategoriesMap = createSelector(
+  [selectCategories],
+  (categories) => {
+    console.log('categoriesMap selector fired');
+    return categories.reduce((acc, category) => {
+      const { title, items } = category;
+      acc[title.toLowerCase()] = items;
+      return acc;
+    }, {});
+  }
+);
