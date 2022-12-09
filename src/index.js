406376a966e9a47a3c144e02f30059825ef2f76a
shahar.shoshany <shahar9772@gmail.com>
2022-12-09 18:29:19 +0200

    transform cart context into the redux store

--- src/index.js
+++ src/index.js
@@ -3,14 +3,10 @@ import reportWebVitals from './reportWebVitals';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-
 import { Provider } from 'react-redux';
 
-import { CartProvider } from './contexts/cart.context';
 import { store } from './store/store';
-
 import App from './App';
-
 import './index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -18,9 +14,7 @@ root.render(
   // <React.StrictMode>
   <Provider store={store}>
     <BrowserRouter>
-      <CartProvider>
-        <App />
-      </CartProvider>
+      <App />
     </BrowserRouter>
   </Provider>
   // </React.StrictMode>
