d8254c9327f85619e914221d9ac95489b72aa88a
shahar.shoshany <shahar9772@gmail.com>
2022-12-10 14:05:25 +0200

    move categories to redux-thunk

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,10 @@
 import { compose, createStore, applyMiddleware } from 'redux';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
-import { loggerMiddleware } from './middleware/logger';
-//import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 
+// import { loggerMiddleware } from './middleware/logger';
 import { rootReducer } from './root-reducer';
 
 const persistConfig = {
@@ -15,7 +16,8 @@ const persistConfig = {
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const middlewares = [
-  process.env.NODE_ENV !== 'production' && loggerMiddleware,
+  process.env.NODE_ENV !== 'production' && logger,
+  thunk,
 ].filter(Boolean);
 
 const composeEnhancer =
